f5632e0579f36a2adb5e969968917812ec2c3db6
BJ Black <bj@wjblack.com>
2025-10-16 00:24:25 -0700

    Add botfunder static website and placeholders (until the app demo is ready).

---
+++ website/index.js
@@ -0,0 +1,8 @@
+// Set up the demo button.
+$( document ).ready(function() {
+    $("#run_demo").click(function() {
+        alert("Not implemented yet.");
+    });
+});
+
+// vim: set tabstop=4 shiftwidth=4 expandtab softtabstop=0 autoindent smarttab:
